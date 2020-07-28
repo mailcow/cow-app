e6c56be9656247836ff9923c65984639678ded14
Ahmet <ahmetkucuk4@gmail.com>
2020-07-29 01:13:05 +0300

    created config.py

---
+++ config.py
@@ -0,0 +1,45 @@
+# *-* coding: utf-8 *-*
+# Ahmet Küçük <ahmetkucuk4@gmail.com>
+# Zekeriya Akgül <zkry.akgul@gmail.com>
+
+DEBUG = True
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+DB = os.environ.get('DATABASE_TYPE','mysql')
+DB_HOST = os.environ.get('DATABASE_HOST','localhost')
+DB_PORT = os.environ.get('DATABASE_PORT','3306')
+DB_USER = os.environ.get('DATABASE_USER','root')
+DB_PASSWORD = os.environ.get('DATABASE_PASSWD','123')
+DB_NAME = os.environ.get('DATABASE_NAME', 'mailcow')
+
+IS_SSL = (os.environ.get('SSL', False) == "true")
+
+# Authentication 
+JWT_ACCESS_COOKIE_PATH = '/'
+JWT_REFRESH_COOKIE_PATH = '/'
+JWT_COOKIE_CSRF_PROTECT = False
+JWT_CSRF_IN_COOKIES = False
+JWT_SECRET_KEY = os.environ.get('SECRET_KEY', 'change_me')
+JWT_BLACKLIST_ENABLED = True
+JWT_COOKIE_SECURE = IS_SSL # Only allow JWT cookies to be sent over https. In production, this should likely be True
+JWT_TOKEN_LOCATION =  ('headers', 'cookies')
+JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+
+# SMTP
+SMTP_HOST = os.environ.get('SMTP_HOST', 'postfix')
+SMTP_PORT = os.environ.get('SMTP_PORT', 25)
+SMTPS_PORT = os.environ.get('SMTPS_PORT', 465)
+
+# IMAP
+IMAP_HOST = os.environ.get('IMAP_HOST', 'dovecot')
+IMAP_PORT = os.environ.get('IMAP_PORT', 143)
+IMAPS_PORT = os.environ.get('IMAPS_PORT', 993)
+
+if DB == 'sqlite':
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, DB_NAME + '.db')
+else:
+    SQLALCHEMY_DATABASE_URI = DB + '://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + ':' + str(DB_PORT) + '/' + DB_NAME
