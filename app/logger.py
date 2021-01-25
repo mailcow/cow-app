b46236d59749d021cccf22b7da9f48f1cfc7da29
Ahmet <ahmetkucuk4@gmail.com>
2021-01-26 00:21:52 +0300

    Changed logger format

--- app/logger.py
+++ app/logger.py
@@ -1,7 +1,8 @@
 import logging
 
-LOGGER_NAME = "cow-app"
-LOGGER_FORMAT = "[%(asctime)s] [%(ident)s] [%(levelname)s] %(message)s"
-DATE_FORMAT = "%Y-%m-%d %H:%M:%S %z"
+LOGGER_NAME = 'cow-app'
+LOGGER_FORMAT = '%(process)d %(asctime)s-%(levelname)s: %(message)s'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S %z'
 
+logging.basicConfig(format=LOGGER_FORMAT, datefmt=DATE_FORMAT)
 logger = logging.getLogger(LOGGER_NAME)
