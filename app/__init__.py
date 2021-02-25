18070231b8ece4b31aa0b766d9a98ac5d86a73d9
Ahmet <ahmetkucuk4@gmail.com>
2021-02-25 22:16:03 +0300

    Fixed sieve template path problem

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # *-* coding: utf-8 *-*
 # Ahmet Küçük <ahmetkucuk4@gmail.com>
 # Zekeriya Akgül <zkry.akgul@gmail.com>
+import os
 
 from flask import Flask
 from flask_restful import Api
@@ -10,7 +11,9 @@ from flask_cors import CORS
 from app.api.exceptions import exceptions
 
 # Define aplication
-app = Flask(__name__)
+APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH = os.path.join(APP_PATH, 'app/sieve_templates/')
+app = Flask(__name__, template_folder=TEMPLATE_PATH)
 app.config.from_object('config')
 
 # Define CORS
